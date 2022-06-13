fceabc8e895a4f266e3ebdd10db228c2a6387660
betul <betbil@gmail.com>
2022-06-13 06:12:11 +0300

    mod and sum (#1)
    * mod and sum
    
    * consumer added

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/betbil/go_rabbit
+
+go 1.18
+
+require github.com/streadway/amqp v1.0.0
--- main.go
+++ main.go
@@ -2,18 +2,62 @@ package main
 
 import (
 	"fmt"
-
 	"github.com/streadway/amqp"
+	"os"
 )
 
 func main() {
+	//argsWithProg := os.Args
+	argsWithoutProg := os.Args[1]
 	fmt.Println("Go RabbitMQ Tutorial")
 	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
 	if err != nil {
-		fmt.Println(err)
-		panic(1)
+		fmt.Println("Failed Initializing Broker Connection")
+		panic(err)
 	}
-	defer conn.Close()
 
-	fmt.Println("Successfully Connected to our RabbitMQ Instance")
+	// Let's start by opening a channel to our RabbitMQ instance
+	// over the connection we have already established
+	ch, err := conn.Channel()
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer ch.Close()
+
+	// with this channel open, we can then start to interact
+	// with the instance and declare Queues that we can publish and
+	// subscribe to
+	q, err := ch.QueueDeclare(
+		"TestQueue",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	// We can print out the status of our Queue here
+	// this will information like the amount of messages on
+	// the queue
+	fmt.Println(q)
+	// Handle any errors if we were unable to create the queue
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// attempt to publish a message to the queue!
+	err = ch.Publish(
+		"",
+		"TestQueue",
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte("Hello World" + argsWithoutProg),
+		},
+	)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Successfully Published Message to Queue")
 }
